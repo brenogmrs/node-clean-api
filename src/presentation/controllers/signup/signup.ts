fc4af319c24001ba16b765888161ee62d393981d
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-04-28 15:32:20 -0300

    feat: ensure signup route returns an account on success

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -49,7 +49,6 @@ export class SignUpController implements Controller {
 
             return ok(account);
         } catch (error) {
-            console.error(error);
             return serverError();
         }
     }

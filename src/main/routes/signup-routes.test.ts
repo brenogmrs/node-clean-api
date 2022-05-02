2f0c45142abd40f8d0b104b7ca26b00344e1a10d
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-05-02 13:01:53 -0300

    test: ensure MongoDb reconnects if client connection is down

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -12,7 +12,7 @@ describe('SignUp Routes context', () => {
     });
 
     beforeEach(async () => {
-        const accountCollection = MongoHelper.getCollection('accounts');
+        const accountCollection = await MongoHelper.getCollection('accounts');
         await accountCollection.deleteMany({});
     });
     test('should return an account on success', async () => {

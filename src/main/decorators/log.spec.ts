27fdbcc97f2f7cf30b180319c456edf140dce0c5
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-05-03 11:15:09 -0300

    refactor: move sut creation to sut method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,29 +1,47 @@
-import { resolve } from 'path';
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols';
 import { LogControllerDecorator } from './log';
 
+interface SutTypes {
+    sut: LogControllerDecorator;
+    controllerStub: Controller;
+}
+
+const makeController = (): Controller => {
+    class ControllerSub implements Controller {
+        async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+            const httpResponse = {
+                statusCode: 200,
+                body: {
+                    email: 'any_email@gmail.com',
+                    name: 'name',
+                    password: '123234',
+                    passwordConfirmation: '123234',
+                },
+            };
+
+            // eslint-disable-next-line no-shadow
+            return new Promise(resolve => resolve(httpResponse));
+        }
+    }
+
+    return new ControllerSub();
+};
+
+const makeSut = (): SutTypes => {
+    const controllerStub = makeController();
+    const sut = new LogControllerDecorator(controllerStub);
+
+    return {
+        sut,
+        controllerStub,
+    };
+};
+
 describe('Log Controller Decorator', () => {
     test('should call controller.handle', async () => {
-        class ControllerSub implements Controller {
-            async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-                const httpResponse = {
-                    statusCode: 200,
-                    body: {
-                        email: 'any_email@gmail.com',
-                        name: 'name',
-                        password: '123234',
-                        passwordConfirmation: '123234',
-                    },
-                };
+        const { sut, controllerStub } = makeSut();
 
-                // eslint-disable-next-line no-shadow
-                return new Promise(resolve => resolve(httpResponse));
-            }
-        }
-
-        const controllerStub = new ControllerSub();
         const handleSpy = jest.spyOn(controllerStub, 'handle');
-        const sut = new LogControllerDecorator(controllerStub);
 
         const httpRequest = {
             body: {
